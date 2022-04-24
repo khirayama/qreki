306f621e93056c6f32d2ca9d08c393e0b9c77649
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2022-04-24 17:51:24 +0900

    style: apply go fmt

--- qreki.go
+++ qreki.go
@@ -6,28 +6,28 @@ import "time"
 type Rokuyo string
 
 type Qreki struct {
-  Year int
-  Month int
-  Day int
-  Uruu bool
-  Rokuyo Rokuyo
-  Mage float64
-  Magenoon float64
-  Illumi float64
-  Mphase int
+	Year     int
+	Month    int
+	Day      int
+	Uruu     bool
+	Rokuyo   Rokuyo
+	Mage     float64
+	Magenoon float64
+	Illumi   float64
+	Mphase   int
 }
 
 func NewQreki(t time.Time) Qreki {
-  qreki := Qreki{
-    Year: 2015,
-    Month: 11,
-    Day: 21,
-    Uruu: false,
-    Rokuyo: "大安",
-    Mage: 0.0,
-    Magenoon: 0.0,
-    Illumi: 0.0,
-    Mphase: 0,
-  }
-  return qreki
+	qreki := Qreki{
+		Year:     2015,
+		Month:    11,
+		Day:      21,
+		Uruu:     false,
+		Rokuyo:   "大安",
+		Mage:     0.0,
+		Magenoon: 0.0,
+		Illumi:   0.0,
+		Mphase:   0,
+	}
+	return qreki
 }
--- qreki_test.go
+++ qreki_test.go
@@ -1,22 +1,22 @@
 package qreki
 
 import (
-  "testing"
-  "reflect"
-  // "fmt"
-  "time"
+	"reflect"
+	"testing"
+	// "fmt"
+	"time"
 )
 
 func TestQreki(t *testing.T) {
-  testcases := []struct {
+	testcases := []struct {
 		in  string
 		out Qreki
 	}{
-    {"2015-12-31", Qreki{2015, 11, 21, false, "大安", 0.0, 0.0, 0.0, 0}},
-  }
+		{"2015-12-31", Qreki{2015, 11, 21, false, "大安", 0.0, 0.0, 0.0, 0}},
+	}
 
-  for _, tc := range testcases {
-    in, _ := time.Parse("2006-01-02", tc.in)
+	for _, tc := range testcases {
+		in, _ := time.Parse("2006-01-02", tc.in)
 		got := NewQreki(in)
 		if !reflect.DeepEqual(got, tc.out) {
 			t.Errorf("%v: got %v, want %v", tc.in, got, tc.out)
